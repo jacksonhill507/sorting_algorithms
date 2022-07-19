e827fb396bdfdd2f85448eb5b5036509115765d9
jacksonhill507 <jacksonchurchill507@gmail.com>
2022-07-19 23:12:46 +0100

    my commit on sorting algorithms

--- deck.h
+++ deck.h
@@ -1,16 +1,21 @@
 #ifndef DECK_H
 #define DECK_H
 
-#include <stdio.h>
 #include <stdlib.h>
 
-/* the structs they gave us */
+/**
+ * enum kind_e - Enumeration of card suits.
+ * @SPADE: Spades suit.
+ * @HEART: Hearts suit.
+ * @CLUB: Clubs suit.
+ * @DIAMOND: Diamonds suit.
+ */
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -22,8 +27,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -35,10 +40,11 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 
 void sort_deck(deck_node_t **deck);
-endif /* SORT_H */
+
+#endif /* DECK_H */
